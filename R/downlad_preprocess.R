fd163b960eca2b5dab0b56760534edcca76e9ebf
spranner5 <133330270+spranner5@users.noreply.github.com>
2023-05-18 20:59:11 +0100

    Update downlad_preprocess.R

--- R/downlad_preprocess.R
+++ R/downlad_preprocess.R
@@ -1,4 +1,4 @@
-# Prep Data
+# Prep / explore Uganda OCDS data
 library(sf)
 library(dplyr)
 library(glue)
@@ -8,80 +8,93 @@ library(jsonlite)
 library(ggplot2)
 library(tidyr)
 install.packages('DT')
+library(sqldf)
 
 # extract bids, awards, buyers info from data 2022 Uganda OCDS contracts
 # data from https://data.open-contracting.org/en/search/
 
 #jsonl format
 
-#test <-read.csv("../data/test.csv")
+#test <-read.csv("../Demonstrator-main/data/test.csv")
 
 lines <- readLines("../data/uganda_releases_2022.jsonl/2022.jsonl")
 lines <- lapply(lines, fromJSON)
 lines <- lapply(lines, unlist)
-x <- bind_rows(lines)
+uganda_data <- bind_rows(lines)
+lines <- NULL
 
-# better name
-uganda_data <- x
-
-# fix numeric values TODO put in col names variables
-uganda_data$bids.details.value.amount <- as.numeric(uganda_data$bids.details.value.amount)
+# rows, cols
+dim(uganda_data)
 
 # TODO sort out the mess of extra data columns
 colnames(uganda_data)
 
+# reduce columns for now by removing any colnames with a number
+pattern_n = "^.+?\\d$"
+uganda_data <- uganda_data %>% select(!(matches(pattern_n)))
 
-# what's in the tags
-uganda_data[1,]$tag
+# explore
+unique(uganda_data$tag) #all same anyway
+unique(uganda_data$awards.value.currency)
+unique(uganda_data$initiationType)
+unique(uganda_data$contracts.value.currency)
+unique(uganda_data$contracts.status)
+unique(uganda_data$tender.status)
+unique(uganda_data$tender.procurementMethod)
 
-#compiled only
+uganda_data <- uganda_data %>% mutate(across(any_of(ends_with("currency")),as.factor))
+uganda_data <- uganda_data %>% mutate(across(any_of(ends_with("amount")),as.numeric))
+uganda_data <- uganda_data %>% mutate(across(any_of(ends_with(".id")),as.numeric))
+uganda_data <- uganda_data %>% mutate(across(any_of(ends_with("status")),as.factor))
+uganda_data <- uganda_data %>% mutate(across(any_of(ends_with("date")),as.Date, format="%Y-%m-%d"))
+uganda_data$tender.procurementMethod <- as.factor(uganda_data$tender.procurementMethod)
 
+# look at numbers
+summary(uganda_data %>% select(where(is.numeric)))
+#lots of NAs, unclear if zeroes mean zero
+# bids_details_value_amount=bids.details.value.amount,
+uganda_data <- rename(uganda_data, bids_details_value_amount=bids.details.value.amount)
+sqldf("select min(bids_details_value_amount) from uganda_data")
+sqldf("select count(*), bids_details_value_amount from uganda_data where bids_details_value_amount < 100 group by bids_details_value_amount")
 
+hist(uganda_data$bids_details_value_amount[uganda_data$bids_details_value_amount < 50000])
 
-
-uganda_data %>%
-  slice_head(n=10) %>%
-  #View()
-  filter(!is.na(bids.details.value.amount)) %>%
-  DT::datatable()
-
-
-
-
-# add months
-uganda_data <- uganda_data %>% mutate(month = month.name[month(date)])
+# add months to explore patterns
+uganda_data <- uganda_data %>% mutate(month = month.name[month(bids.details.date)])
+uganda_data <- uganda_data %>% mutate(year = year(bids.details.date))
 # only Jan though in 23 so use '22 or other
-uganda_data <- uganda_data %>% mutate(day = day.name[day(date)])
 
 # factorise months
 uganda_data$month = factor(uganda_data$month, levels = month.name)
 
+uganda_data_22 <- sqldf("select month, bids_details_value_amount from uganda_data where
+                        year = 2022")
 
+uganda_data_22$month = factor(uganda_data_22$month, levels = month.name)
 
 #uganda_data %>% group_by(month) %>% summarise_each(funs(sum))
-
-by_month <- uganda_data %>%
+by_month <- uganda_data_22 %>%
   group_by(month) %>%
-  filter(!is.na(bids.details.value.amount)) %>%
-  summarize(count=n(), amount = sum(bids.details.value.amount))
+  filter(!is.na(bids_details_value_amount)) %>%
+  summarize(count=n(), amount = sum(bids_details_value_amount))
 #!caution - not all dates are 22!
-
-# all cols
-colnames(uganda_data)
-
-
+by_month
+ 
 # bids
-tibble(x=1:8, y=c('ocid', 'date', 'tag', 'bids.details.id', 'bids.details.date', 'bids.details.value.amount', 'bids.details.value.currency', 'bids.details.status'))
+#tibble(x=1:8, y=c('ocid', 'date', 'tag', 'bids.details.id', 'bids.details.date', 'bids_details_value_amount', 'bids.details.value.currency', 'bids.details.status'))
 
-#by_month
-bids <- uganda_data[ , c('ocid', 'date', 'tag', 'bids.details.id', 'bids.details.date', 'bids.details.value.amount', 'bids.details.value.currency', 'bids.details.status')]
-# type conversions
-bids$bids.details.id <- as.numeric(bids$bids.details.id)
-bids$bids.details.date <- as.Date(bids$bids.details.date, format="%Y-%m-%d")
+bids <- uganda_data[ , c('ocid', 'date', 'tag', 'bids.details.id', 'bids.details.date', 'bids_details_value_amount', 'bids.details.value.currency', 'bids.details.status')]
 
-# drop nas
+# rename
+bids <- rename(bids, bids_details_id=bids.details.id,
+               bids_details_date=bids.details.date,
+               #bids_details_value_amount=bids.details.value.amount,
+               bids_details_value_currency=bids.details.value.currency,
+               bids_details_status=bids.details.status)
+
+# drop nas?
 sum(!complete.cases(bids[-1]))
-bids_complete <- bids %>% drop_na() 
+#bids_complete <- bids %>% drop_na() 
 
 # save bid details by month
 write.csv(bids, "../data/uganda_22_bids.csv")
@@ -93,6 +106,9 @@ awards <- uganda_data[, c('ocid', 'date', 'tag', 'awards.id', 'awards.date',
 
 sum(!complete.cases(awards[-1]))
 awards <- awards %>% drop_na() 
+# rename #TODO function for this
+awards <- rename(awards, awards_id=awards.id, awards_date=awards.date, awards_v_amount=awards.value.amount,
+                 awards_v_currency=awards.value.currency, awards_suppliers_id=awards.suppliers.id, awards_suppliers_name=awards.suppliers.name)
 write.csv(awards, "../data/uganda_22_awards.csv")
 
 # buyers
@@ -101,8 +117,7 @@ buyers <- uganda_data[, c('ocid', 'date', 'buyer.id', 'buyer.name')]
 sum(!complete.cases(buyers[-1]))
 buyers <- buyers %>% drop_na() 
 
-# save data back out
-write.csv(buyers, "../data/uganda_22_buyers.csv")
- 
+buyers <- rename(buyers, buyer_id=buyer.id,buyer_name=buyer.name)
+# TO DO refactor auto rename from dot notation
 
- 
+write.csv(buyers, "../data/uganda_22_buyers.csv")
